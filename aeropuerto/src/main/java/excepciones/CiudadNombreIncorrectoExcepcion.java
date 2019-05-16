d404e87090bfc6e1038956d4e3d3b04ce64587aa
juan asis <juan.asis1234@gmail.com>
2019-05-15 21:46:01 -0300

    se agrega excepcion personalizada ciudad

---
+++ aeropuerto/src/main/java/excepciones/CiudadNombreIncorrectoExcepcion.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class CiudadNombreIncorrectoExcepcion extends CiudadException {
+
+	public CiudadNombreIncorrectoExcepcion() {
+		super("Ciudad Nombre Incorrecto");
+		// TODO Auto-generated constructor stub
+	}
+
+}
